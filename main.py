ad01e8748c2fb8e1efa28fdc5199f5d54682da49
adambb9 <55655523+adambb9@users.noreply.github.com>
2021-09-29 14:34:24 +0200

    style changes tomato image and background blend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import tkinter
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -8,6 +8,10 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+DARKBLUE = "#082032"
+DARKBLUEGREY ="#2C394B"
+BLUEGREY = "#334756"
+BURNTORANGE ="#ff4c29"
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -16,3 +20,31 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
+
+window =tkinter.Tk()
+window.title("Pomodoro App")
+window.config(padx=100, pady=50, bg=DARKBLUE)
+
+canvas = tkinter.Canvas(width=202, height=224, bg=DARKBLUE, highlightthickness=0)
+tomato_image = tkinter.PhotoImage(file=r"pomodoroapp\tomato.png")
+canvas.create_image(100,112, image=tomato_image)
+canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+timer_label = tkinter.Label(text="Timer", font=(FONT_NAME, 40, "bold"), fg=BURNTORANGE, bg=DARKBLUE)
+timer_label.grid(row=0, column=1)
+
+
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
